08866da0daa2df5749184e9138b8f6f2b110b996
keshav <hankavtech@gmail.com>
2020-11-01 05:24:40 +0530

    changed ctrl paths1

--- src/main/java/com/hankav/controller/Login.java
+++ src/main/java/com/hankav/controller/Login.java
@@ -27,7 +27,7 @@ import com.hankav.model.User;
 /**
  * Servlet implementation class Login
  */
-@WebServlet("/sportbabas/Login")
+@WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private String hashedpass;
